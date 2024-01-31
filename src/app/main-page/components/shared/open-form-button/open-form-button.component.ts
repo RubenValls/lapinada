b3f46223f438ec9ac36ff6a41f2d9d2243e01ccf
Rubén Valls <r.vallsaparici@gmail.com>
2024-01-31 20:40:43 +0100

    add some components. styles and header

---
+++ src/app/main-page/components/shared/open-form-button/open-form-button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-open-form-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './open-form-button.component.html',
+  styleUrl: './open-form-button.component.scss'
+})
+export class OpenFormButtonComponent {
+
+}
