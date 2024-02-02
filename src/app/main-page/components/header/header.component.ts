9db1d1f553e65b8180fe274ca380a89051852b79
Rubén Valls <r.vallsaparici@gmail.com>
2024-02-02 13:52:39 +0100

    add redirection to ubicacion page

--- src/app/main-page/components/header/header.component.ts
+++ src/app/main-page/components/header/header.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
 import { OpenFormButtonComponent } from '../shared/open-form-button/open-form-button.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
-    OpenFormButtonComponent
+    OpenFormButtonComponent,
+    RouterModule,
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
 }
