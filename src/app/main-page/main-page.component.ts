949a9de0b9f6e8d5c6c6dd6fcc590b44093b9581
Rubén Valls <r.vallsaparici@gmail.com>
2024-02-02 13:32:07 +0100

    add google maps component

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { ImagesSliderComponent } from './components/images-slider/images-slider.component';
 import { HeroTextComponent } from './components/hero-text/hero-text.component';
+import { GoogleMapComponent } from './components/google-map/google-map.component';
 
 @Component({
   selector: 'app-main-page',
@@ -9,7 +10,8 @@ import { HeroTextComponent } from './components/hero-text/hero-text.component';
   imports: [
     HeaderComponent,
     ImagesSliderComponent,
-    HeroTextComponent
+    HeroTextComponent,
+    GoogleMapComponent,
   ],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
