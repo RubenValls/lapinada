46740400317f12ed8c1be2837190e660053264d0
Rubén Valls <r.vallsaparici@gmail.com>
2024-02-03 18:43:46 +0100

    la pinada web first version finished

--- src/app/main-page/components/shared/list-item/list-item.component.ts
+++ src/app/main-page/components/shared/list-item/list-item.component.ts
@@ -11,6 +11,7 @@ export class ListItemComponent {
 
   @Input() summary: string | undefined;
   @Input() text: string | undefined;
+  @Input() index: string | undefined;
 
   constructor() {}
 
--- src/app/main-page/components/shared/open-form-button/open-form-button.component.ts
+++ src/app/main-page/components/shared/open-form-button/open-form-button.component.ts
@@ -9,4 +9,9 @@ import { Component } from '@angular/core';
 })
 export class OpenFormButtonComponent {
 
+  onHandleClick(){
+    window.scrollTo(0, document.body.scrollHeight)
+  }
+
 }
+
